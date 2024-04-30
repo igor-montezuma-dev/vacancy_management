4a5097ee3711d3404f3248d9ba585ffafa14209c
IgorMontezuma20 <igorminranda17@gmail.com>
2024-04-29 21:39:05 -0300

    feat: Adding candidate service layer in the application

--- src/main/java/com/igormontezumadev/gestao_vagas/module/candidate/controllers/CandidateController.java
+++ src/main/java/com/igormontezumadev/gestao_vagas/module/candidate/controllers/CandidateController.java
@@ -1,10 +1,10 @@
 package com.igormontezumadev.gestao_vagas.module.candidate.controllers;
 
-import com.igormontezumadev.gestao_vagas.exceptions.UserFoundException;
 import com.igormontezumadev.gestao_vagas.module.candidate.CandidateEntity;
-import com.igormontezumadev.gestao_vagas.module.candidate.CandidateRepository;
+import com.igormontezumadev.gestao_vagas.module.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
 
     @Autowired
-    private CandidateRepository candidateRepository;
+    private CreateCandidateUseCase createCandidateUseCase;
+
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity){
-        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
-                .ifPresent((user) -> {
-                    throw new UserFoundException("Usuário já cadastrado.");
-                });
-        return this.candidateRepository.save(candidateEntity);
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
---
+++ src/main/java/com/igormontezumadev/gestao_vagas/module/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,22 @@
+package com.igormontezumadev.gestao_vagas.module.candidate.useCases;
+
+import com.igormontezumadev.gestao_vagas.exceptions.UserFoundException;
+import com.igormontezumadev.gestao_vagas.module.candidate.CandidateEntity;
+import com.igormontezumadev.gestao_vagas.module.candidate.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCandidateUseCase {
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public CandidateEntity execute(CandidateEntity candidateEntity) {
+        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException("Usuário já cadastrado.");
+                });
+        return this.candidateRepository.save(candidateEntity);
+
+    }
+}
