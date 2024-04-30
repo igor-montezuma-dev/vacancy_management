a51ecf6be7c87e2174375bab795329db4fa85cba
IgorMontezuma20 <igorminranda17@gmail.com>
2024-04-30 13:05:20 -0300

    feat: Add createCompamny service in the application

---
+++ src/main/java/com/igormontezumadev/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -0,0 +1,22 @@
+package com.igormontezumadev.gestao_vagas.modules.company.useCases;
+
+import com.igormontezumadev.gestao_vagas.exceptions.UserFoundException;
+import com.igormontezumadev.gestao_vagas.modules.company.entities.CompanyEntity;
+import com.igormontezumadev.gestao_vagas.modules.company.repositories.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCompanyUseCase {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public CompanyEntity execute(CompanyEntity companyEntity) {
+        this.companyRepository.findByUsernameOrEmail(companyEntity.getUserName(), companyEntity.getEmail())
+                .ifPresent((company) -> {
+                    throw new UserFoundException("Usuário já cadastrado!");
+                });
+        return this.companyRepository.save(companyEntity);
+    }
+}
