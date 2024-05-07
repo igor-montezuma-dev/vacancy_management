3e67deb364e67792a36cd34c8bacc0c1b019c0a9
IgorMontezuma20 <igorminranda17@gmail.com>
2024-05-06 22:41:59 -0300

    feat: Add enconding the candidate password

--- src/main/java/com/igormontezumadev/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/com/igormontezumadev/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,18 +4,24 @@ import com.igormontezumadev.gestao_vagas.exceptions.UserFoundException;
 import com.igormontezumadev.gestao_vagas.modules.candidate.entities.CandidateEntity;
 import com.igormontezumadev.gestao_vagas.modules.candidate.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException("Usuário já cadastrado.");
                 });
+
+        var password = this.passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
         return this.candidateRepository.save(candidateEntity);
 
     }
